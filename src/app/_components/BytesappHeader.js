54130c85ce3abc6656185ed7cad1bc6e6379aaf5
honeygt3rs <harshitshrivastava169@gmail.com>
2024-11-08 15:40:55 +0530

    Create Header

--- src/app/_components/BytesappHeader.js
+++ src/app/_components/BytesappHeader.js
@@ -1,21 +1,16 @@
 import Link from 'next/link'
 const BytesappHeader = () => {
     return (
-        <div>
+        <div className='headerContainer'>
             <div className="logo">
                 <img style={{ width: 100 }} src="https://www.bytefoods.co/wp-content/uploads/2017/05/Byte-Foods-Logo-GREEN.png" />
             </div>
-            <ul>
-                <li>
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/">Login/SignUp</Link>
-                </li>
-                <li>
-                    <Link href="/">Profile</Link>
-                </li>
-            </ul>
+            <div>
+                <Link href="/">Home</Link>
+                <Link className='headerItem' href="/">Login/SignUp</Link>
+                <Link href="/">Profile</Link>
+            </div>
+
         </div>
     )
 }
